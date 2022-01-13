2c5bfc713124020e9426a795a2fa109361bd4726
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-01-14 00:13:26 +0700

    create back button component

---
+++ src/components/Back.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './back.scss';
+
+const Back = (props) => {
+    console.log(props.theme);
+
+    return (
+        <div className="back">
+            <div className={`wrapper ${props.theme === "dark" ? "dark" : ""}`}>
+                <Link to="/" className="button">
+                    <FontAwesomeIcon icon={['fas', 'long-arrow-alt-left']} />
+                    <span>Back</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Back;
